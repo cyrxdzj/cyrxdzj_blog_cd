6f58b88895faa2c625dd9d3830bcff0deaa4df96
cyrxdxzj <2824671438@qq.com>
2026-06-16 11:04:02 +0800

    完善首页文章列表

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Col, ConfigProvider, Flex, notification, Row } from "antd";
+import React, { useState } from 'react';
+import { Col, ConfigProvider, Flex, notification, Pagination, Row } from "antd";
 import { AntdConfigProvider_light } from "../utils/utils";
-import { Background, Text, Card } from "../CyrxDesign/Components";
+import { Background, Text, Card, NextLine } from "../CyrxDesign/Components";
 import card_002_035_normal from "../media/background/card_002_035_normal.webp";
 import MainLogo from "../media/common/main_logo.png";
 
@@ -9,6 +9,10 @@ function IndexPage(props) {
     document.title="cyrxdzj的博客";
     const [notificationAPI, contextHolder] = notification.useNotification();
     console.log(props.index_yaml);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 10;
+    const startIndex = (currentPage - 1) * pageSize;
+    const currentPosts = props.index_yaml.posts.slice(startIndex, startIndex + pageSize);
     return (
         <ConfigProvider theme={AntdConfigProvider_light}>
             {contextHolder}
@@ -30,6 +34,27 @@ function IndexPage(props) {
                                 <Col span={16}><a href="https://luogu.com.cn/user/387836"><Text>https://luogu.com.cn/user/387836</Text></a></Col>
                             </Row>
                         </Card>
+                    </Col>
+                    <Col span={16}>
+                        <Flex vertical gap={8}>
+                            {currentPosts.map(post => (
+                                <a href={`/post/${post.id}`} target='_blank'>
+                                    <Card key={post.id}>
+                                        <Text type={"h3"}>{post.title}</Text>
+                                        <NextLine/>
+                                        <Text>{post.summary}</Text>
+                                    </Card>
+                                </a>
+                            ))}
+                            <Flex justify="center">
+                                <Pagination
+                                    current={currentPage}
+                                    total={props.index_yaml.posts.length}
+                                    pageSize={pageSize}
+                                    onChange={(page) => setCurrentPage(page)}
+                                />
+                            </Flex>
+                        </Flex>
                     </Col>
                 </Row>
             </Background>
